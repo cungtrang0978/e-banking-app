62fb4b40f126d8fa89e9a32ed340228ee63802b2
thangnv-hybrid <thangnv@hybrid-technologies.vn>
2022-08-29 15:53:48 +0700

    feat: update ui

--- app/src/main/java/com/example/e_banking_app/ui/balance/BalanceFragment.kt
+++ app/src/main/java/com/example/e_banking_app/ui/balance/BalanceFragment.kt
@@ -4,34 +4,84 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.e_banking_app.R
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.e_banking_app.data.factory.TransactionViewModelFactory
 import com.example.e_banking_app.databinding.FragmentBalanceBinding
 
+/**
+ * A fragment representing a list of Items.
+ */
 class BalanceFragment : Fragment() {
 
-    private var _binding: FragmentBalanceBinding? = null
+    private var columnCount = 1
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+
+    private lateinit var viewModel: BalanceViewModel
+    private var _binding: FragmentBalanceBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this)[BalanceViewModel::class.java]
-R.string.info
         _binding = FragmentBalanceBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(
+            this,
+            TransactionViewModelFactory(context!!)
+        )[BalanceViewModel::class.java]
+
+        binding.rcvBalance.apply {
+            layoutManager = when {
+                columnCount <= 1 -> LinearLayoutManager(context)
+                else -> GridLayoutManager(context, columnCount)
+            }
+        }
+        binding.loading.visibility = View.VISIBLE
 
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.balanceResult.observe(viewLifecycleOwner) { result ->
+            result ?: return@observe
+            binding.loading.visibility = View.GONE
+
+            result.success?.let {
+                binding.rcvBalance.adapter = BalanceRecyclerViewAdapter(it)
+            }
+            result.error?.let {
+                Toast.makeText(context?.applicationContext, it, Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            BalanceFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/e_banking_app/ui/balance/BalanceViewModel.kt
+++ app/src/main/java/com/example/e_banking_app/ui/balance/BalanceViewModel.kt
@@ -3,8 +3,25 @@ package com.example.e_banking_app.ui.balance
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.e_banking_app.R
+import com.example.e_banking_app.data.repository.TransactionRepository
 
-class BalanceViewModel : ViewModel() {
+class BalanceViewModel(private val transactionRepository: TransactionRepository) : ViewModel() {
+    private val _balanceResult = MutableLiveData<BalanceResult>()
+    val balanceResult: LiveData<BalanceResult> = _balanceResult
 
+    init {
+        fetchBalanceList()
+    }
 
+    private fun fetchBalanceList() {
+        transactionRepository.getBalanceList(
+            onSuccess = {
+                _balanceResult.value = BalanceResult(success = it)
+            },
+            onFailure = {
+                _balanceResult.value = BalanceResult(error = R.string.fail_to_fetch_balance)
+            }
+        )
+    }
 }
